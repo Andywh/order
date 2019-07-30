63231aade93e0b7566394829451e4ff7164d0255
songliang <liang.song@ximalaya.com>
2019-07-30 16:29:20 +0800

    order 多模块拆分

--- server/src/main/java/com/joy/order/OrderApplication.java
+++ server/src/main/java/com/joy/order/OrderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.joy.order.client")
 public class OrderApplication {
 
     public static void main(String[] args) {
